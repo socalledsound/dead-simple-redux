fc04cc6b9d9c7da1bede7cf6568866f488364952
chris kubick <lrs6000@gmail.com>
2021-02-02 02:01:15 -0800

    single page with two actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-// import { createStore } from 'redux'
-// import { Provider } from 'react-redux'
+ import { createStore } from 'redux'
+ import { Provider } from 'react-redux'
+
+ const INITIAL_STATE = {
+     x: 500,
+     y: 20,
+     color: 'purple',
+ }
+
+const myReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type){
+        case 'SET_NEW_POSITION':
+            return {...state, x: action.payload.x, y: action.payload.y} 
+        case 'SET_NEW_COLOR' :
+            return { ...state, color: action.payload.color}
+        default: return state
+    }
+
+}
 
 
+const store = createStore(myReducer)
+
+
+const setMousePosition = ({ x, y }) => {
+    return ({
+        type: 'SET_NEW_POSITION',
+        payload: {
+            x, y
+        }
+    })
+}
+
+const setNewColor = (color) => {
+    console.log(color);
+    return ({
+        type: 'SET_NEW_COLOR',
+        payload: {
+            color
+        }
+    })
+}
 
 
 const bgStyle = {
@@ -14,7 +52,6 @@ const bgStyle = {
 };
 
 const circleStyle = {
-    backgroundColor : 'blue', 
     width: '50px', 
     height: '50px', 
     borderRadius: '25px', 
@@ -23,26 +60,41 @@ const circleStyle = {
 class App extends Component {
     
     state = {
-        x: 500,
-        y: 250,
+        x: 0,
+        y: 0,
+        color: 'pink',
     }
 
 
     componentDidMount(){
         window.addEventListener('mousemove', this.onMouseMove)
+
+        store.subscribe(() => {
+            const state = store.getState();
+            this.setState({x : state.x, y: state.y, color: state.color });
+        })
     }
 
     componentWillUnmount(){
         window.removeEventListener('mousemove', this.onMouseMove)
     }
 
+
+    changeColor = () => {
+        const randomColor = `#${Math.floor(Math.random()* 16777215).toString(16)}`;
+        store.dispatch(setNewColor(randomColor));
+    }
+
     onMouseMove = (e) => {
-        this.setState({ x: e.clientX - 25, y: e.clientY -25 });
+        //this.setState({ x: e.clientX - 25, y: e.clientY -25 });
+        store.dispatch(setMousePosition({ x: e.clientX - 25, y: e.clientY -25 }))
+   
     }
 
 
     render(){
-        const { x, y } = this.state;
+        const { x, y, color } = this.state;
+        console.log(color);
         return (
             <div style={bgStyle}> 
             <div style={
@@ -50,8 +102,12 @@ class App extends Component {
                     ...circleStyle, 
                     left : `${x}px`,
                     top: `${y}px`,
+                    backgroundColor : color, 
                     }
-                }>
+                }
+                
+                onClick={this.changeColor}    
+                >
 
             </div>
         </div>
@@ -64,6 +120,10 @@ class App extends Component {
 
 
 ReactDOM.render(
-    <App />,
+    
+    <Provider store={store} >
+        <App />
+    </Provider>
+    ,
     document.getElementById('root')
 )
